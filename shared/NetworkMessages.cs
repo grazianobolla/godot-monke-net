b8454d68179c4c43da881ac5f4467308c3d37738
Graziano Bolla <grazianobolla12@gmail.com>
2023-02-06 12:54:19 -0300

    formatted NetworkMessages.cs

--- shared/NetworkMessages.cs
+++ shared/NetworkMessages.cs
@@ -12,60 +12,41 @@ namespace NetMessage
     [MessagePackObject]
     public partial struct Sync : ICommand
     {
-        [Key(0)]
-        public int ClientTime;
-
-        [Key(1)]
-        public int ServerTime;
+        [Key(0)] public int ClientTime;
+        [Key(1)] public int ServerTime;
     }
 
     // Encapsulates user input and other client actions
     [MessagePackObject]
     public partial struct UserCommand : ICommand
     {
-        [Key(0)]
-        public int Id;
-
-        [Key(1)]
-        public UserInput[] Commands;
+        [Key(0)] public int Id;
+        [Key(1)] public UserInput[] Commands;
     }
 
     [MessagePackObject]
     public partial struct UserInput
     {
-        [Key(0)]
-        public int Stamp; //TODO: OPTIMIZE TO NOT SEND MULTIPLE STAMPS! only one needed
-
-        [Key(1)]
-        public byte Keys;
+        [Key(0)] public int Stamp; //TODO: is not necessary to send the stamp in every input
+        [Key(1)] public byte Keys;
     }
 
     // Game state for a given point in time
     [MessagePackObject]
     public partial struct GameSnapshot : ICommand
     {
-        [Key(0)]
-        public UserState[] States;
-
-        [Key(1)]
-        public int Time;
+        [Key(0)] public UserState[] States;
+        [Key(1)] public int Time;
     }
 
     // Encapsulates current state for a player (gets sent with gameSnapshot)
     [MessagePackObject]
     public partial struct UserState
     {
-        [Key(0)]
-        public int Id; // Player ID
-
-        [Key(1)]
-        public float[] PosArray; // Player position
-
-        [Key(2)]
-        public float[] VelArray; // Player velocity
-
-        [Key(3)]
-        public int Stamp; // Last processed stamp
+        [Key(0)] public int Id; // Player ID
+        [Key(1)] public float[] PosArray; // Player position
+        [Key(2)] public float[] VelArray; // Player velocity
+        [Key(3)] public int Stamp; // Last processed stamp
 
         [IgnoreMember]
         public Vector3 Position
