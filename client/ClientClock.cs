0c8fb84015e7dbe4774a30fe168c6f1e54ad82eb
Graziano Bolla <grazianobolla12@gmail.com>
2024-03-14 17:03:22 -0300

    added log every time latency calculations are done

--- client/ClientClock.cs
+++ client/ClientClock.cs
@@ -48,12 +48,6 @@ public partial class ClientClock : Node
     public void ProcessTick()
     {
         _currentTick += 1 + _lastOffset;
-
-        if (_lastOffset != 0)
-        {
-            GD.Print($"At Tick: {_currentTick - (_lastOffset + 1)} Adjusted local clock by: {_lastOffset} ticks");
-        }
-
         _lastOffset = 0;
     }
 
@@ -107,6 +101,8 @@ public partial class ClientClock : Node
             _averageLatencyInTicks = SmoothAverage(_latencyValues, _minLatencyInTicks);
 
             EmitSignal(SignalName.LatencyCalculated, _averageLatencyInTicks, _jitterInTicks);
+
+            GD.Print($"At tick {_currentTick}, latency calculations done. Avg. Latency {_averageLatencyInTicks} ticks, Jitter {_jitterInTicks} ticks, Clock Offset {_lastOffset} ticks");
 
             _offsetValues.Clear();
             _latencyValues.Clear();
