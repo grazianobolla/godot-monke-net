b3627ef6915730a86aa182290c9685d7f953cb36
Graziano Bolla <grazianobolla@gmail.com>
2024-08-26 11:26:29 -0300

    Create StateRollbacker.cs

---
+++ addons/monke-net/src/ClientSide/StateRollbacker.cs
@@ -0,0 +1,12 @@
+﻿namespace MonkeNet.Client;
+
+/// <summary>
+/// Handles going back in time to a certain tick and resimulating the world state forward again.
+/// </summary>
+public partial class StateRollbacker : ClientNetworkNode
+{
+    protected override void OnProcessTick(int currentTick, int currentRemoteTick)
+    {
+
+    }
+}
